a401473c665379bc2242e2b20113bb27e933c23c
Celia Chen <celia@zergling.net>
2009-01-10 02:00:21 -0500

    Addressed Victor's comments in previous commit.

--- lib/yahoo_fetcher.rb
+++ lib/yahoo_fetcher.rb
@@ -2,27 +2,22 @@ require 'net/https'
 require 'uri'
 require 'csv'
 
-# Pulls stock information from Yahoo Finance
-#  Parameter:
-#    search_stock_data takes in an array of stock tickers 
-#    ex. ["GOOG","MSFT"] 
-#
-#  Output:
-#    an array of hashes with each hash accounting for one of the stock tickers  
-#    ex. [{:ask=>316.18, :bid=>315.0}, {:ask=>19.6, :bid=>19.58}]
-
-
 module YahooFetcher
-  def self.search_stock_data(tickers)
-    data_csv = fetch_yahoo_data(tickers)
+  def self.stock_data_for_tickers(tickers)
+    data_csv = fetch_data(tickers)
     data_parsed = parse_response(data_csv)
   end
 
-#  Yahoo uses different codes to pull data for stock tickers ("a" = Ask, "e" = Earnings per share, etc.)
-#  Tickers are entered into an array and then joined with "+"
-#  The output of fetching Yahoo data is a CSV document with each company on a separate line  
-  def self.fetch_yahoo_data(tickers)
-    data_codes = "ab"
+  # Pulls and returns stock data from Yahoo finance
+  #  Parameters:
+  #    different codes to pull data for stock tickers 
+  #      ex. "a" = Ask, "e" = Earnings per share, etc.
+  #    an array of stock tickers 
+  #      ex. ["GOOG","MSFT"] 
+  #  Output:
+  #    CSV document with each company's data on a separate line  
+  def self.fetch_data(tickers)
+    data_codes = "abx"
     
     query = "/d/?s=#{URI.encode(tickers.join("+"))}&f=#{URI.encode(data_codes)}"
     
@@ -33,16 +28,24 @@ module YahooFetcher
     return response.body
   end
 
-# The CSV file outputted from above is now parsed and converted into an array of hashes  
+  # The CSV file outputted from above is now parsed and converted into 
+  #    an array of hashes with each hash accounting for one of the stock tickers  
+  #    ex. [{:ask=>316.18, :bid=>315.0}, {:ask=>19.6, :bid=>19.58}]  
   def self.parse_response(response)
+    print response
     results = CSV.parse(response)
  
     results.map do |result|
+      if result[2] != "N/A"
       { :ask => result[0].to_f,
-        :bid => result[1].to_f
+        :bid => result[1].to_f,
+        :stock_exchange => result[2]
       }
+      else
+        :not_found
+      end
     end 
   end
 end
-
-# p YahooFetcher.search_stock_data(["GOOG","MSFT"])
+ p YahooFetcher.fetch_data(["GOOG","XXXX","MSFT","ZZZZ"])
+ p YahooFetcher.stock_data_for_tickers(["GOOG","XXXX","MSFT","ZZZZ"])
--- test/unit/yahoo_fetcher_test.rb
+++ test/unit/yahoo_fetcher_test.rb
@@ -2,34 +2,31 @@ require 'test_helper'
 
 class YahooFetcherTest < ActiveSupport::TestCase
   def test_pull_Yahoo_data
-    @tickers = ["GOOG","MSFT"]
-    fetched_data = YahooFetcher.fetch_yahoo_data(@tickers)
+    @tickers = ["GOOG","XXXX","MSFT"]
+    fetched_data = YahooFetcher.fetch_data(@tickers)
     assert_not_nil fetched_data, "No data was fetched"
     parsed_data = YahooFetcher.parse_response(fetched_data)
     assert_equal @tickers.length, parsed_data.length, "Incorrect data fetched"
   end
   
-  # pulling data with incorrect tickers will return a row with 0's
-  def test_pull_Yahoo_data_with_incorrect_tickers
-    @tickers = ["ZZZZ"]
-    fetched_data = YahooFetcher.fetch_yahoo_data(@tickers)
-    assert_not_nil fetched_data
-  end
-  
   def test_pull_Yahoo_data_without_tickers_fails
     @tickers = nil
     assert_raise(NoMethodError) do
-      fetched_data = YahooFetcher.fetch_yahoo_data(@tickers)
+      fetched_data = YahooFetcher.fetch_data(@tickers)
     end
   end
   
   def test_parse_csv_to_hash
     fetched_data = <<END
-315.71,314.70
-19.59,19.56
+N/A,312.50,"NasdaqNM"
+N/A,N/A,"N/A"
+N/A,19.03,"NasdaqNM"
+N/A,N/A,"AMEX"
 END
-    golden_tickers = [{:ask => 315.71, :bid => 314.70},
-    {:ask => 19.59, :bid => 19.56}]
+    golden_tickers = [{:ask => 0.0, :bid => 312.50, :stock_exchange => "NasdaqNM"},
+    :not_found,
+    {:ask => 0.0, :bid => 19.03, :stock_exchange => "NasdaqNM"},
+    {:ask => 0.0, :bid => 0.0, :stock_exchange => "AMEX"}]
 
     tickers = YahooFetcher.parse_response fetched_data
     
