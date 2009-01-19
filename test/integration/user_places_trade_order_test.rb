eb3a1e0feb4b044babfa64d6a319bdb8a021e61c
Celia Chen <celia@zergling.net>
2009-01-18 19:29:54 -0500

    Addressed Victor's comments - round 2.

--- test/integration/user_places_trade_order_test.rb
+++ test/integration/user_places_trade_order_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UserPlacesTradeOrderTest < ActionController::IntegrationTest
   fixtures :all
 
-  # User decides to create account and portfolio on the website. 
-  # User creates a new user name and password in the login box.  
+  # User decides to create a new account on the website. An associated portfolio is automatically created. 
+  # User enters a user name and password in the login sign up form and clicks create.  
   # User logs in.
   # User views his portfolio and decides to place an order.
   # User creates and places a new order.
@@ -12,7 +12,6 @@ class UserPlacesTradeOrderTest < ActionController::IntegrationTest
   # User is redirected to his portfolio and sees the new order in a list of trade orders.
   # User finally logs out.
   test "user places trade order" do
-    get "/"
     get "/users/new"
     assert_response :success
     
@@ -21,27 +20,27 @@ class UserPlacesTradeOrderTest < ActionController::IntegrationTest
     assert_response :success
     assert_equal "User #{@user.name} was successfully created.", flash[:notice]
     
-    assert_not_nil User.find(:first, :conditions => {:name => "bunny"})
-    @portfolio = Portfolio.find(:first, :conditions => {:user_id => @user.id})
+    assert_not_nil @user
+    @portfolio = @user.portfolio
     assert_not_nil @portfolio
     
     post_via_redirect "/admin/login", :name => @user.name, :password => "carrot"
     assert_response :success
     assert_equal @user.id, session[:user_id]
     
-    assert_equal 0, TradeOrder.find(:all, :conditions => {:portfolio_id => @user.portfolio.id}).length
+    assert_equal 0, @user.portfolio.trade_orders.length
     assert_equal 250000.00, @portfolio.cash
     
     get "/trade_orders/new"
     assert_response :success
     
-    post "trade_orders/", :trade_order => {:is_limit => false, 
-                                                        :ticker => "MS", 
-                                                        :quantity => 40, 
-                                                        :is_buy => true,
-                                                        :is_long => true}
+    post "/trade_orders", :trade_order => {:is_limit => false, 
+                                           :ticker => "MS", 
+                                           :quantity => 40, 
+                                           :is_buy => true,
+                                           :is_long => true}
     assert_redirected_to "/portfolios/#{@portfolio.id}"
-    assert 1, TradeOrder.find(:all, :conditions => {:portfolio_id => @user.portfolio.id}).length
+    assert 1, @user.portfolio.trade_orders.length
     
     get "/admin/logout"
     assert_redirected_to "/admin/login"
