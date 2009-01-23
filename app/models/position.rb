98da4684f172e8b4b3be5589e7c9e2b5fcc66968
Victor Costan <victor@zergling.net>
2009-01-22 20:40:18 -0500

    Comments explaining the meaning of the attributes in the Position model.

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,16 +1,21 @@
 class Position < ActiveRecord::Base
   belongs_to :portfolio
   belongs_to :stock
-  
+
+  # The stock held in this position.
   validates_numericality_of :stock_id, :greater_than => 0, :allow_nil => false
   
+  # The portfolio containing this position.
   validates_numericality_of :portfolio_id, :greater_than => 0, :allow_nil => false
   
-  validates_inclusion_of :is_long, :in => [true, false], :allow_nil => false
-  
+  # The number of stocks in this position.
   validates_numericality_of :quantity, :greater_than => 0, :allow_nil => false
   
+  # The average base cost of this position.
+  # We haven't quite decided what this means yet.
   validates_numericality_of :average_base_cost, :allow_nil => false
   
+  # True for long positions, false for shorts.
+  validates_inclusion_of :is_long, :in => [true, false], :allow_nil => false
   validates_uniqueness_of :is_long, :scope => [:portfolio_id, :stock_id]
 end
